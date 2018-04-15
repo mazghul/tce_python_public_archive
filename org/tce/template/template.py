98c52d91bee46d857eb900fcf0a9d17ba5b4bbc2
rajacsp <raja.csp@gmail.com>
2018-04-15 10:19:00 -0400

    Py template folder/file added

---
+++ org/tce/template/template.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# the above line is to avoid 'SyntaxError: Non-UTF-8 code starting with' error
+
+'''
+author:
+    [author name(s) should come here]
+
+source: (if any)
+    If you followed any tutorial, please paste the link so everyone will benefit from it
+    
+    sample:
+    https://pythonspot.com/python-set/
+    
+'''
+
+def test():
+    
+    return "template test method"
+
+
+def main():
+    print('Main')
+    print(test())
+
+if __name__ == '__main__':
+    main()
